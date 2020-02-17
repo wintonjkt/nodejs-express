de961c5c201c40dde3f6aeebe4a57a49e8a4ebce
wintonjkt <wintonjkt@gmail.com>
2020-02-17 05:52:43 -0800

    m

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody Demo");
+  res.send("Hello from IBM<br>");
+  res.send("<br>");
+  res.send("My name is Winton and we can help you with your microservices journey<br>");
 });
  
 module.exports.app = app;
