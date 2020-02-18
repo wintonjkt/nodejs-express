115a6ff19a93d8108b519b303e1b5ee681624931
wintonjkt <wintonjkt@gmail.com>
2020-02-17 20:56:09 -0800

    m

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const app = require('express')()
 
-app.get('/', (req, res) => {
-  res.send("Hello from IBM<br>");
-  res.send("<br>");
-  res.send("My name is Winton and we can help you with your microservices journey<br>");
+app.get('/:name', (req, res) => {
+  res.send("Hello from " + req.params.name);
 });
  
 module.exports.app = app;
