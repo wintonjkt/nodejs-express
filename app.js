1f7b20918fa6828b7658b80175cb7e3b2eefd8ca
wintonjkt <wintonjkt@gmail.com>
2020-03-29 17:16:45 -0700

    m

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/:name', (req, res) => {
-  res.send("Hello test demo wish you have a great day " + req.params.name);
+  res.send("IBM wish you have a great day " + req.params.name);
 });
  
 module.exports.app = app;
